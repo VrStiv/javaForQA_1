d65f678b6a0c0b8ef66b54828b103dda4c00be56
Stanislav Trubchanov <vrstiv@gmail.com>
2016-02-21 15:27:19 +0200

    Changes to formatting code

--- sendbox/src/main/java/ru/stqa/javaForQA_1/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/javaForQA_1/sandbox/MyFirstProgram.java
@@ -3,9 +3,7 @@ package ru.stqa.javaForQA_1.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-
-System.out.println("Hello, world!");
-
+    System.out.println("Hello, world!");
   }
 
 }
