f4beff1b465115bfe44f8f3c03c3fd49de72c833
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-02 21:40:20 +0200

    Revert "Revert "End of part 11""
    This reverts commit f42217827e6e8b4db65146d21a7f527a9b875bb8.

--- addressbook-web-mytests/src/test/java/ru/stqa/pft/myaddressbook/appmanager/UserHelper.java
+++ addressbook-web-mytests/src/test/java/ru/stqa/pft/myaddressbook/appmanager/UserHelper.java
@@ -4,39 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.myaddressbook.model.UserData;
 
-public class UserHelper {
-  private FirefoxDriver wd;
+public class UserHelper extends HelperBase {
 
   public UserHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToMainPage() {
-      wd.findElement(By.linkText("home page")).click();
+    click(By.linkText("home page"));
   }
 
   public void submitForm() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillUserForm(UserData userData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(userData.getFirstName());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(userData.getLastName());
-      wd.findElement(By.name("nickname")).click();
-      wd.findElement(By.name("nickname")).clear();
-      wd.findElement(By.name("nickname")).sendKeys(userData.getNickName());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(userData.getPhoneHome());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(userData.getMailHome());
-      wd.findElement(By.name("address2")).click();
-      wd.findElement(By.name("address2")).clear();
-      wd.findElement(By.name("address2")).sendKeys(userData.getAddressHome());
+    type(By.name("firstname"), userData.getFirstName());
+    type(By.name("lastname"), userData.getLastName());
+    type(By.name("nickname"), userData.getNickName());
+    type(By.name("home"), userData.getPhoneHome());
+    type(By.name("email"), userData.getMailHome());
+    type(By.name("address2"), userData.getAddressHome());
   }
+
 }
