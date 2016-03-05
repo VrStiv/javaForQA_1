d6f4deafe2f3c046f39689693767feb4afa37dd5
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-05 23:55:57 +0200

    Running tests in different browsers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,22 +2,38 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private UserHelper userHelper;
+  private String browser;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    System.out.println("Используется браузер " + browser );
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://127.0.0.1:8080/addressbook/");
     groupHelper = new GroupHelper(wd);
@@ -26,7 +42,6 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
-
 
 
   public void stop() {
---
+++ sendbox/src/main/java/ru/stqa/javaForQA_1/sandbox/Equality.java
@@ -0,0 +1,13 @@
+package ru.stqa.javaForQA_1.sandbox;
+
+/* Created by Vrstiv on 05.03.2016  */
+public class Equality {
+  public static void main(String[] args) {
+    String s1 = "firefox";
+    String s2 = "firefox";
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+
+  }
+}
