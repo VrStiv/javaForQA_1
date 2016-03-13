9d12677af450a8f0e38883c3c7da5ca32330d5d0
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-13 22:43:20 +0200

    Formation of the collection All

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactModificationTests extends TestBase {
 
   @Test
@@ -16,15 +18,15 @@ public class ContactModificationTests extends TestBase {
       // true в параметрах - для модификации, когда присутствует элемент "Group"
       app.getContactHelper().createContact(new ContactData("Stas", "Trubchanov", "Vrstiv", "+3806777777777", "stas.trubchanov@mail.ru", "Kharkiv", "StasTest1"), true);
     }
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     // Параметр для выбора первого элемента (первый способ из лекции)
-    app.getContactHelper().initContactModification(before + 1);
+    app.getContactHelper().initContactModification(before.size() + 1);
     // null в параметрах - значение по умолчанию
     // false в параметрах - для модификации, когда отсутствует элемент "Group"
     app.getContactHelper().fillContactForm(new ContactData("Stasm", "Trubchanovm", "Vrstivm", "+3806777777799", "stasm.trubchanov@mail.ru", "Kharkiv", null), false);
     app.getContactHelper().modificationUser();
     app.getContactHelper().returnToMainPage();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
