a8c2bf5bb4bd5b8baac18376a16620b02412f40f
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-14 22:10:04 +0200

    8 part for All

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
-  private final String id;
+  private int id;
   private final String firstName;
   private final String lastName;
   private final String nickName;
@@ -10,7 +10,7 @@ public class ContactData {
   private final String addressHome;
   private String group;
 
-  public ContactData(String id, String firstName, String lastName, String nickName, String phoneHome, String mailHome, String addressHome, String group) {
+  public ContactData(int id, String firstName, String lastName, String nickName, String phoneHome, String mailHome, String addressHome, String group) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
@@ -21,22 +21,9 @@ public class ContactData {
     this.group = group;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
-
-  public ContactData(String firstName, String lastName, String nickName, String phoneHome, String mailHome, String addressHome, String group) {
-    this.id = null;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.nickName = nickName;
-    this.phoneHome = phoneHome;
-
-    this.mailHome = mailHome;
-    this.addressHome = addressHome;
-    this.group = group;
-  }
-
 
   @Override
   public boolean equals(Object o) {
@@ -45,7 +32,7 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != null ? !id.equals(that.id) : that.id != null) return false;
+    if (id != that.id) return false;
     if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
     return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
 
@@ -53,11 +40,29 @@ public class ContactData {
 
   @Override
   public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
+    int result = id;
     result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
     result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
     return result;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public ContactData(String firstName, String lastName, String nickName, String phoneHome, String mailHome, String addressHome, String group) {
+    this.id = 0;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.phoneHome = phoneHome;
+
+    this.mailHome = mailHome;
+    this.addressHome = addressHome;
+
+    this.group = group;
+  }
+
 
   @Override
   public String toString() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -19,10 +21,15 @@ public class ContactCreationTests extends TestBase {
     // Проверка наличия нового элемента
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
+    int max = 0;
+    for (ContactData g : after){
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    contact.setId(max);
     before.add(contact);
-
-
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
