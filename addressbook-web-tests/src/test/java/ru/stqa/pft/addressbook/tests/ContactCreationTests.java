29bc053651b564eff1cbae3112651dc70f60cf75
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-20 02:36:46 +0200

    Task №11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,26 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test (enabled = true)
+  @Test(enabled = true)
   public void testUserCreation() {
-    Set<ContactData> before = app.contact().all();
+    Contacts before = app.contact().all();
     app.goTo().addContactPage();
-    // true в параметрах - для модификации, когда присутствует элемент "Group"
     ContactData contact = new ContactData()
             .withFirstName("Stas").withLastName("Trubchanov").withNickName("Vrstiv")
             .withPhoneHome("+3806777777777").withMailHome("stas.trubchanov@mail.ru").withAddressHome("Kharkiv").withGroup("StasTest1");
     app.contact().create(contact, true);
-    Set<ContactData> after = app.contact().all();
+    Contacts after = app.contact().all();
     // Проверка наличия нового элемента
-    Assert.assertEquals(after.size(), before.size() + 1);
-
-    contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    assertThat(after.size(), equalTo(before.size() + 1));
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
-
 }
