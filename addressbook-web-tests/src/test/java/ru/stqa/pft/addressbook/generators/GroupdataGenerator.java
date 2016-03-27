a9f0ed301614521e904b5fc1191fd982cef6d750
Stanislav Trubchanov <vrstiv@gmail.com>
2016-03-27 20:29:16 +0300

    The parameterization of test methods

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupdataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupdataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;/* Created by Vrstiv on 26.03.2016  */
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,18 +14,32 @@ import java.util.List;
 
 public class GroupdataGenerator {
 
+  @Parameter (names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
-
-
-
+    GroupdataGenerator generator = new GroupdataGenerator();
+    JCommander jCommander = new JCommander(generator);
+     try {
+       jCommander.parse(args);
+     } catch (ParameterException ex) {
+       jCommander.usage();
+       return;
+     }
+    generator.run();
+    //int count = Integer.parseInt(args[0]);
+    //File file = new File(args[1]);
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group: groups){
@@ -31,7 +48,7 @@ public class GroupdataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++){
       groups.add(new GroupData().withName(String.format("test %s", i))
@@ -39,5 +56,4 @@ public class GroupdataGenerator {
     }
     return groups;
   }
-  
 }
